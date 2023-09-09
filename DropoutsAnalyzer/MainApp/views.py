4659f863509aef1bc18b7e96f6fac70c10c0af98
= <=>
2023-09-09 15:52:53 +0530

    Added all total counts

--- DropoutsAnalyzer/MainApp/views.py
+++ DropoutsAnalyzer/MainApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from Report.models import Student
+from Report.models import Student,School,Dropout
 import requests
+from datetime import date
 
 
 
@@ -37,10 +38,19 @@ class ChartPage(View):
         castes_data=api.request_data(request.build_absolute_uri('/filter/caste'))
         cities_data=api.request_data(request.build_absolute_uri('/filter/city'))
         
+        total_students=Student.objects.count()
+        total_schools=School.objects.count()
+
+        start_date = date(2023, 1, 1)
+        end_date = date(2023, 12, 31)
+        total_dropouts=Dropout.objects.filter(date__range=(start_date, end_date)).count()
         context = {
             'states_data': states_data['results'],
             'castes_data': castes_data['results'],
             'cities_data': cities_data['results'],
+            'total_students':total_students,
+            'total_schools':total_schools,
+            'total_dropouts':total_dropouts
         }
         return render(request,self.template_name,context)
 
